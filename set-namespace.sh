876f4e52f1aeff1f9380ab35d7d36395e4bb89e9
donal <donalspring@gmail.com>
2020-04-08 15:18:45 +0100

    🧝‍♀️ Update to readmes and such 🥴

---
+++ set-namespace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ./set-namespace.sh $ci_cd_namespace $dev_namespace $test_namespace
+if [ -z ${1} ] || [ -z ${2} ] || [ -z ${3} ]; then 
+  echo "🤥 No namespaces specified - please set them 🤥 "
+  echo "./set-namespace.sh \$ci_cd_namespace \$dev_namespace \$test_namespace"
+  echo "For example:"
+  echo "./set-namespace.sh my-ci-cd my-dev my-test"
+  exit -1
+fi
+
+sed -i '' -e "s#\"labs-ci-cd\"#\"${1}\"#s" bootstrap/values-bootstrap.yaml
+sed -i '' -e "s#\"labs-dev\"#\"${2}\"#s" bootstrap/values-bootstrap.yaml
+sed -i '' -e "s#\"labs-test\"#$\"{3}\"#s" bootstrap/values-bootstrap.yaml
+
+sed -i '' -e "s#labs-dev#${2}#g" example-deployment/values-applications.yaml
+
+sed -i '' -e "s#labs-ci-cd#${1}-#g" ubiquitous-journey/values-tooling.yaml
+
+echo "🐙 All done - happy helming 🐙"
